d1ac1a5d8a258019a8473303e580e633de080cf6
James Geib <james@zuse.com>
2023-09-06 15:17:34 -0400

    implemented react-redux logic and prototypical structure to Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
-import { Header } from "semantic-ui-react";
+import { Header, Card, Image, Button, Grid } from "semantic-ui-react";
+import { connect } from "react-redux";
 
 class Home extends Component {
     render() {
+        let itemList = this.props.items.map(item => {
+            return (
+                <Grid.Column width={4} key={item.id}>
+                    <Card >
+                        <div id="card-img-container">
+                            <Image src={item.img} alt={item.title} />
+                            <Card.Header>{item.title}</Card.Header>
+                            <Button to="/"><Icon name="plus" /> Add</Button>
+                        </div>
+                        <Card.Content>
+                            <p>{item.desc}</p>
+                            <p>Price: {item.price}</p>
+                        </Card.Content>
+                    </Card>
+                </Grid.Column>
+            );
+        });
         return (
             <>
-                <Header as="h3">Home</Header>
+                <Grid container>
+                    <Grid.Row>
+                        {itemList}
+                    </Grid.Row>
+                </Grid>
             </>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        items: state.items
+    }
+}
+
+export default connect(mapStateToProps)(Home);
