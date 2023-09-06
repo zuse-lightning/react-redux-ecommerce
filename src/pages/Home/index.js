9eab55e1f1634716e2a336bd1def57fcde8b87f2
James Geib <james@zuse.com>
2023-09-06 14:43:43 -0400

    constructed initial Home Page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Header } from "semantic-ui-react";
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+                <Header as="h3">Home</Header>
+            </>
+        );
+    }
+}
+
+export default Home;
