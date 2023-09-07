4ca116169fb213ea3e2c7cd570b3eb9c08b720a5
James Geib <james@zuse.com>
2023-09-07 14:35:56 -0400

    added types and actions for remove, add, and subtract functions

--- src/components/actions/cartActions.js
+++ src/components/actions/cartActions.js
@@ -1,9 +1,40 @@
-import { ADD_TO_CART } from "./types/types"
+import { 
+    ADD_TO_CART,
+    REMOVE_ITEM,
+    ADD_QUANTITY,
+    SUB_QUANTITY,
+    ADD_SHIPPING,
+    SUB_SHIPPING 
+} from "./types/types"
 
 export const addToCart = (id) => {
     console.log("Added to cart");
     return {
         type: ADD_TO_CART,
+        id: id
+    }
+}
+
+export const removeItem = (id) => {
+    console.log("Removed from cart");
+    return {
+        type: REMOVE_ITEM,
+        id: id
+    }
+}
+
+export const addQuantity = (id) => {
+    console.log("Added");
+    return {
+        type: ADD_QUANTITY,
+        id: id
+    }
+}
+
+export const subtractQuantity = (id) => {
+    console.log("subtracted");
+    return {
+        type: SUB_QUANTITY,
         id: id
     }
 }
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -2,8 +2,22 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { Header, List, Image, Icon, Button, Grid } from "semantic-ui-react";
+import { removeItem, addQuantity, subtractQuantity } from "../../components/actions/cartActions";
 
 class Cart extends Component {
+
+    handleRemove = (id) => {
+        this.props.removeItem(id);
+    }
+
+    handleAddQuantity = (id) => {
+        this.props.addQuantity(id);
+    }
+
+    handleSubtractQuantity = (id) => {
+        this.props.subtractQuantity(id);
+    }
+
     render() {
 
         let addedItems = this.props.items.length ?
@@ -21,10 +35,10 @@ class Cart extends Component {
                                     <p><b>Price: {item.price}</b></p>
                                     <p><b>Quantity: {item.quantity}</b></p>
                                     <div className="add-remove">
-                                        <Link to="/cart"><Icon name="arrow up" /></Link>
-                                        <Link to="/cart"><Icon name="arrow down" /></Link>
+                                        <Link to="/cart"><Icon onClick={() => this.handleAddQuantity(item.id)} name="arrow up" /></Link>
+                                        <Link to="/cart"><Icon onClick={() => this.handleSubtractQuantity(item.id)} name="arrow down" /></Link>
                                     </div>
-                                    <Button color="red" className="remove">Remove</Button>
+                                    <Button onClick={() => this.handleRemove(item.id)} color="red" className="remove">Remove</Button>
                                 </List.Description>
                             </List.Item>
                         </>
@@ -53,4 +67,12 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps)(Cart);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        removeItem: (id) => { dispatch(removeItem(id)) },
+        addQuantity: (id) => { dispatch(addQuantity(id)) },
+        subtractQuantity: (id) => { dispatch(subtractQuantity(id)) }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
