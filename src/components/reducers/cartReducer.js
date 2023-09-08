6bd908ea606403f80646ae56508edddc0013fe74
James Geib <james@zuse.com>
2023-09-08 11:56:00 -0400

    minor tweaks

--- src/components/reducers/cartReducer.js
+++ src/components/reducers/cartReducer.js
@@ -15,12 +15,12 @@ import {
 
 let INITIAL_STATE = {
     items: [
-        { id: 1, title: "Women's Lab Coat", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 110, img: LabCoat },
-        { id: 2, title: "Coffee Tumbler", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 80, img: CoffeeTumbler },
-        { id: 3, title: "Javalina Purple Pen", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 120, img: PurplePen },
-        { id: 4, title: "Umbrella", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 260, img: Umbrella },
-        { id: 5, title: "Socks", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 160, img: Socks },
-        { id: 6, title: "Javalina Sky Blue Pen", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price: 90, img: SkyBluePen },
+        { id: 1, title: "Women's Lab Coat", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", quantity: 0, price: 110, img: LabCoat },
+        { id: 2, title: "Coffee Tumbler", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", quantity: 0, price: 80, img: CoffeeTumbler },
+        { id: 3, title: "Javalina Purple Pen", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", quantity: 0, price: 120, img: PurplePen },
+        { id: 4, title: "Umbrella", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", quantity: 0, price: 260, img: Umbrella },
+        { id: 5, title: "Socks", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", quantity: 0, price: 160, img: Socks },
+        { id: 6, title: "Javalina Sky Blue Pen", desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", quantity: 0, price: 90, img: SkyBluePen },
     ],
     addedItems: [],
     total: 0
@@ -30,6 +30,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 
     if (action.type === ADD_TO_CART) {
         let addedItem = state.items.find(item => item.id === action.id);
+        console.log(addedItem);
         let existed_item = state.addedItems.find(item => action.id === item.id);
         if (existed_item) {
             addedItem.quantity += 1;
